76fbc49af0cf4bfd39499ed39febe4212439ccec
Mak-12345 <101962337+Mak-12345@users.noreply.github.com>
2022-03-20 15:03:34 +0530

    Add files via upload

---
+++ EVEN_ODD.java
@@ -0,0 +1,22 @@
+package MARCH_16;
+import java.util.*;
+public class EVEN_ODD 
+{
+	public static void main(String args[])
+	{
+		int a;
+		Scanner s=new Scanner(System.in);
+		
+		System.out.println("enter an no:");
+		a=s.nextInt();
+		
+		if(a%2==0)
+			System.out.println(a+"is a even no");
+		else
+			System.out.println(a+"is a odd no");
+			
+			
+		
+	}
+  
+}
